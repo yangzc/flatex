76ae5aa49823d1564dcbe7a6d6ba7816dcd0c934
JiangXL <lionofafu@gmail.com>
2020-08-21 11:16:22 +0800

    上标下标构造器

---
+++ lib/macro/sub_macro.dart
@@ -0,0 +1,24 @@
+///
+/// Copyright (C) 2020 The flatex Project
+/// @author jiangxl on 2020/08/19.
+///
+import 'package:flatex/atom.dart';
+import 'package:flatex/macro/macro_info.dart';
+import 'package:flatex/parse/tex_formula.dart';
+import 'package:flatex/parse/tex_parser.dart';
+import 'package:flutter/material.dart';
+
+class SubMacroInfo extends MacroInfo {
+  Alignment position = Alignment.center;
+  @override
+  SubMacroInfo(this.position);
+  Atom buildAtom(TexParser tp, List<String> args) {
+    TeXFormula num = new TeXFormula(tp, args[0]);
+    if (num.root == null)
+      throw new Exception(
+          "Both numerator and denominator of a fraction can't be empty!");
+    Atom atom = num.root;
+    atom.position = position;
+    return atom;
+  }
+}
